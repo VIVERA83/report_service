3fdb2a0779b420978353e6dbbc3de09f5f73698b
vivera83 <vivera83@yandex.ru>
2025-05-26 16:32:27 +0300

    убраны не используемые данные

--- main.py
+++ main.py
@@ -3,14 +3,3 @@ from app.service.cli import CLIRouter
 if __name__ == "__main__":
     client = CLIRouter()
     client.run()
-
-
-# from app.core.setup import Reports
-# from app.core.utils import args_parser
-#
-# if __name__ == "__main__":
-#     result = args_parser()
-#     report = Reports()
-#     result = report.handler(result.files, report=result.report)
-#     if result:
-#         print(result)
